917e30f52f6d394dd37c48f9fa1c1fc0b8579b19
yaolya <galyagina.olya@gmail.com>
2022-12-24 02:07:04 +0300

    Initial Commit

---
+++ ios-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-calculator
+//
+//  Created by Оля Галягина on 24.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
